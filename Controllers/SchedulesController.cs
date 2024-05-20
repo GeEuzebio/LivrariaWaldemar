3c683608ac5f2264899f4db2c425e59013280e19
GeEuzebio <georgeeuzebio@gmail.com>
2024-05-20 12:11:09 -0300

    adjust on schedules

--- Controllers/SchedulesController.cs
+++ Controllers/SchedulesController.cs
@@ -8,7 +8,6 @@ namespace LibraryApp.Controllers
 {
     public class SchedulesController : Controller
     {
-        List<Schedule> ListOfSchedules = [];
         private readonly ApplicationDbContext _context;
 
         public SchedulesController(ApplicationDbContext context)
@@ -16,11 +15,20 @@ namespace LibraryApp.Controllers
             _context = context;
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Search(DateTime date)
+        {
+            return View(nameof(Index), await _context.Scheduling.FirstOrDefaultAsync(d => d.Day == date.ToUniversalTime()));
+        }
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Schedule.ToListAsync());
+            DateTime toDay = new DateTime(DateTime.Now.Year,DateTime.Now.Month, DateTime.Now.Day);
+            Scheduling? schedule = await _context.Scheduling.FirstOrDefaultAsync(d => d.Day == toDay.ToUniversalTime());
+            ViewBag.Schedule = schedule;
+            return View();
         }
-
+        [Route("pesquisa_por_data")]
         public async Task<IActionResult> SearchByDate(DateTime date)
         {
             Scheduling? schedules = await _context.Scheduling.FirstOrDefaultAsync(s => s.Day!.Value.ToUniversalTime() == date.ToUniversalTime());
@@ -47,15 +55,29 @@ namespace LibraryApp.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> CreateSchedule([Bind("Day,ClassHour,Professor,classRoom")] Schedule schedule)
+        public async Task<IActionResult> CreateSchedule(DateTime day, ClassRoom classRoom, Professionals professor, int pos)
         {
-            if(ModelState.IsValid)
+            bool exist = await ScheduleExist(day.ToUniversalTime());
+            if (exist)
             {
-                _context.Schedule.Add(schedule);
+                Scheduling? schedule = await _context.Scheduling.FirstOrDefaultAsync(d => d.Day!.Value.ToUniversalTime() == day.ToUniversalTime());
+                schedule.Professor[pos] = professor;
+                schedule.ClassRoom[pos] = classRoom;
+                schedule.IsScheduled[pos] = true;
+                _context.Scheduling.Update(schedule);
                 await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
             }
-            return View();
+            else
+            {
+                Scheduling newSchedule = new();
+                newSchedule.Day = day.ToUniversalTime();
+                newSchedule.Professor[pos] = professor;
+                newSchedule.ClassRoom[pos] = classRoom;
+                newSchedule.IsScheduled[pos] = true;
+                _context.Scheduling.Add(newSchedule);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
         }
 
         [HttpPost]
@@ -68,6 +90,28 @@ namespace LibraryApp.Controllers
             };
             ViewBag.Position = position;
             return View(schedule);
+        }
+
+        [HttpPut]
+        [ValidateAntiForgeryToken]
+        //TODO
+        public async Task<IActionResult> Edit()
+        {
+            return View(await _context.Scheduling.ToListAsync());
+        }
+
+        [HttpDelete]
+        [ValidateAntiForgeryToken]
+        //TODO
+        public async Task<IActionResult> Delete()
+        {
+            return View(await _context.Scheduling.ToListAsync());
+        }
+
+        private async Task<bool> ScheduleExist(DateTime? day)
+        {
+            bool result = await _context.Scheduling.FirstOrDefaultAsync(x => x.Day == day) is null ? false : true;
+            return result;
         }
     }
 }
--- Models/Professionals.cs
+++ Models/Professionals.cs
@@ -1,7 +1,24 @@
+using System.ComponentModel;
+
 namespace LibraryApp.Models
 {
     public enum Professionals
     {
-        
+        [Description("Renê")]
+        Rene,
+        [Description("Laura")]
+        Laura
+    }
+}
+public static class EnumExtensions
+{
+    public static string GetDescription(this Enum value)
+    {
+        var fieldInfo = value.GetType().GetField(value.ToString());
+
+        var descriptionAttributes = (DescriptionAttribute[])fieldInfo!.GetCustomAttributes(
+            typeof(DescriptionAttribute), false);
+
+        return descriptionAttributes.Length > 0 ? descriptionAttributes[0].Description : value.ToString();
     }
 }
