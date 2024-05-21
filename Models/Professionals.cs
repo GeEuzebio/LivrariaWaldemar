7629543ee8de32fa622743b1dc4effa281d01520
GeEuzebio <georgeeuzebio@gmail.com>
2024-05-21 16:51:51 -0300

    Professionals added on Schedules

--- Models/Professionals.cs
+++ Models/Professionals.cs
@@ -4,12 +4,52 @@ namespace LibraryApp.Models
 {
     public enum Professionals
     {
-        [Description("Renê")]
-        Rene,
+        [Description("Artenia")]
+        Artenia,
+        [Description("Carol")]
+        Carol,
+        [Description("Cristiane")]
+        Cristiane,
+        [Description("Davi")]
+        Davi,
+        [Description("Elias")]
+        Elias,
+        [Description("Fred")]
+        Fred,
+        [Description("Gabriela")]
+        Gabriela,
+        [Description("Gerlucio")]
+        Gerlucio,
+        [Description("Gleidson")]
+        Gleidson,
+        [Description("Iracema")]
+        Iracema,
+        [Description("Jessica")]
+        Jessica,
         [Description("Laura")]
         Laura,
+        [Description("Leones")]
+        Leones,
+        [Description("Ligonardo")]
+        Ligonardo,
+        [Description("Marcos")]
+        Marcos,
+        [Description("Nara")]
+        Nara,
         [Description("ONHB")]
-        ONHB
+        ONHB,
+        [Description("Otilia")]
+        Otilia,
+        [Description("Paulo")]
+        Paulo,
+        [Description("PC")]
+        PC,
+        [Description("Regiane")]
+        Regiane,
+        [Description("Renne")]
+        Renne,
+        [Description("Sandro")]
+        Sandro
     }
 }
 public static class EnumExtensions
